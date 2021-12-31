b2a6ec989108238134607be8c291f111a533eb50
EikE Natan Sousa Brito <eike.sousa@hotmail.com>
2021-12-31 12:56:11 -0300

    connection db

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,4 +1,13 @@
-/* eslint-disable no-return-await */
-import { createConnection } from 'typeorm';
+import { createConnection, getConnectionOptions } from 'typeorm';
 
-(async () => await createConnection())();
+interface IOptions {
+	host: string;
+}
+
+getConnectionOptions().then(options => {
+	const newOptions = options as IOptions;
+	newOptions.host = 'database';
+	createConnection({
+		...options,
+	});
+});
