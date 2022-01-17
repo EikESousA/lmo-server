570068acfc3dba1eaa8ab058f8e2979f13d78bad
EikE Natan Sousa Brito <eike.sousa@hotmail.com>
2022-01-16 22:32:23 -0300

    test

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,9 +1,13 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 
 import { usersRoutes } from './users.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRoutes);
+
+routes.get('/test', (request: Request, response: Response) => {
+	response.json({ text: 'hello word' });
+});
 
 export { routes };
